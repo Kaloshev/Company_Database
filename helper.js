d09fbac8c2bde52838325f958bf79bee2941d006
Gjorgji Kaloshev <49526774+Kaloshev@users.noreply.github.com>
2019-12-11 01:04:06 +0100

    Email Validator modification

--- helper.js
+++ helper.js
@@ -1,9 +1,12 @@
-emailValidator = (email) => {
-    if (email.length > 5) {
-        return true
-    } else {
-        return false
+emailValidator = (req, res, next) => {
+    let email = req.body.email
+    var validEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if (!validEmail.test(email)) {
+        var error = new Error("Email is not valid!");
+        error.status = 400;
+        next(error);
     }
+    next();
 };
 
 nameValidator = (name) => {
